80cb2c27d602c0b9eada4a19ae95fea76b73488c
Geovany Gameros <geovanygameros@hotmail.com>
2016-10-20 16:55:46 -0600

    First commit

---
+++ app/src/main/java/com/example/geovany/android_arduino/ControlFragment.java
@@ -0,0 +1,23 @@
+package com.example.geovany.android_arduino;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by geovany on 10/20/16.
+ */
+
+public class ControlFragment extends Fragment{
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.control_layout, container, false);
+        return myView;
+    }
+}
